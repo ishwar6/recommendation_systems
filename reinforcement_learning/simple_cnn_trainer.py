896cbf9633b43b71ea0613b2dd7539c844bcc775
ishwar6 <iisudrj11@gmail.com>
2025-08-05 19:34:52 +0530

    Train CNN on MNIST dataset

---
+++ reinforcement_learning/simple_cnn_trainer.py
@@ -0,0 +1,51 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+class SimpleCNN(nn.Module):
+    """A simple convolutional neural network for image classification."""
+    def __init__(self):
+        super(SimpleCNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.fc1 = nn.Linear(64 * 7 * 7, 128)
+        self.fc2 = nn.Linear(128, 10)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.relu = nn.ReLU()
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, x):
+        x = self.pool(self.relu(self.conv1(x)))
+        x = self.pool(self.relu(self.conv2(x)))
+        x = x.view(-1, 64 * 7 * 7)
+        x = self.relu(self.fc1(x))
+        x = self.softmax(self.fc2(x))
+        return x
+
+def train_model():
+    """Train the CNN model on the MNIST dataset."""
+    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+    trainset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+    trainloader = DataLoader(trainset, batch_size=64, shuffle=True)
+
+    model = SimpleCNN()
+    criterion = nn.NLLLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    for epoch in range(5):
+        running_loss = 0.0
+        for images, labels in trainloader:
+            optimizer.zero_grad()
+            output = model(images)
+            loss = criterion(output, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+        print(f'Epoch {epoch+1}, Loss: {running_loss/len(trainloader)}')
+
+    return model
+
+if __name__ == '__main__':
+    trained_model = train_model()
